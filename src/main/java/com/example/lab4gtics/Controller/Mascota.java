aa90d344fbe902cac5f9550a988a8819d050337d
david34Y <dgonzalesn@pucp.edu.pe>
2022-09-27 20:37:27 -0500

    Controller y REpository creados

---
+++ src/main/java/com/example/lab4gtics/Controller/Mascota.java
@@ -0,0 +1,10 @@
+package com.example.lab4gtics.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mascota")
+public class Mascota {
+
+}
---
+++ src/main/java/com/example/lab4gtics/Repository/MascotaRepository.java
@@ -0,0 +1,4 @@
+package com.example.lab4gtics.Repository;
+
+public interface MascotaRepository {
+}
