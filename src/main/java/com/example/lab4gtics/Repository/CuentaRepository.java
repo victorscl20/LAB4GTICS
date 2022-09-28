046e7adfe5b3723615200b740740f05acf59e647
Manuel <a20190133@pucp.edu.pe>
2022-09-27 21:23:41 -0500

    sadasdasd

---
+++ src/main/java/com/example/lab4gtics/Repository/CuentaRepository.java
@@ -0,0 +1,15 @@
+package com.example.lab4gtics.Repository;
+
+import com.example.lab4gtics.dto.CuentasDto;
+import com.example.lab4gtics.entity.Cuenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CuentaRepository  extends JpaRepository<Cuenta, Integer> {
+    @Query(nativeQuery = true, value = "select c.correo, c.direccion,c.telefono,count(idmascota) as 'numero' from cuenta c\n" +
+            "left join mascota m on m.cuenta_idcuenta = c.idcuenta\n" +
+            "group by c.idcuenta;")
+    List<CuentasDto> listaCuenta ();
+}
---
+++ src/main/java/com/example/lab4gtics/dto/CuentasDto.java
@@ -0,0 +1,8 @@
+package com.example.lab4gtics.dto;
+
+public interface CuentasDto {
+    String getCorreo();
+    String getDireccion();
+    String getTelefono();
+    int getNumero();
+}
