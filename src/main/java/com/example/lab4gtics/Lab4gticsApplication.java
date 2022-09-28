f10cb0908ccd4b243fafb60c27af98aa34311c1d
Victor Calderon <victor.calderon@pcup.edu.pe>
2022-09-27 19:52:17 -0500

    cuack

---
+++ src/main/java/com/example/lab4gtics/Lab4gticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab4gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4gticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4gticsApplication.class, args);
+    }
+
+}
